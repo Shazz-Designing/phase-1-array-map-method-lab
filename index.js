b9cd8932cb29b06cf039aad0cf533746af5051a3
sharon <sapot@safaricom.co.ke>
2023-10-19 00:05:14 +0300

    Add code for titlecase

--- index.js
+++ index.js
@@ -12,5 +12,13 @@ const tutorials = [
 ];
 
 const titleCased = () => {
-  return tutorials
+  return tutorials.map(function (tutorial) {
+    const words = tutorial.split(" ");
+    const titleCasedWords = words.map(function (word) {
+      return word.charAt(0).toUpperCase() + word.slice(1);
+    });
+    return titleCasedWords.join(" ");
+  });
 }
+
+console.log(titleCased());
